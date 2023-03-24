0d8d9d29bb8dd980a5506ff6106759cc21478dae
Carlos Alves <diego.rodrigues@cubos.io>
2023-03-24 17:42:01 -0300

    feat: add new lib expo reanimated

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,40 +1,66 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  View,
+} from "react-native";
 
-import { THEME } from '../../styles/theme';
-import { styles } from './styles';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+import { THEME } from "../../styles/theme";
+import { styles } from "./styles";
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
-  HARD: THEME.COLORS.DANGER_LIGHT,
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
-}
+  HARD: THEME.COLORS.DANGER_LIGHT,
+};
 
 type Props = TouchableOpacityProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
-}
+};
 
-export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
+export function Level({
+  title,
+  type = "EASY",
+  isChecked = false,
+  ...rest
+}: Props) {
+  const scale = useSharedValue(1);
+  const animetedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
 
   const COLOR = TYPE_COLORS[type];
 
   return (
     <TouchableOpacity {...rest}>
-      <View style={
-        [
+      <Animated.View
+        style={[
           styles.container,
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
-        ]
-      }>
-        <Text style={
-          [
+          animetedContainerStyle,
+          {
+            borderColor: COLOR,
+            backgroundColor: isChecked ? COLOR : "transparent",
+          },
+        ]}
+      >
+        <Text
+          style={[
             styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-          ]}>
+            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR },
+          ]}
+        >
           {title}
         </Text>
-      </View>
+      </Animated.View>
     </TouchableOpacity>
   );
 }
